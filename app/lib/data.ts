8d2165cfcac3a34a019b032b61099347dd35350d
John Agudelo <johnaagudelo@gmail.com>
2023-11-15 21:54:08 -0500

    Add loading component and update dashboard page layout

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,12 +21,12 @@ export async function fetchRevenue() {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+    console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -58,6 +58,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
